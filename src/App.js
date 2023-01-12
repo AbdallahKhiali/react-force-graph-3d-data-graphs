b5ea7d2d4b51603bca9c55b46ad3f6752632df28
AbdallahKhiali <work.khiali@gmail.com>
2023-01-12 13:15:26 +0100

    data visualisation react force 3d graph

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState, useRef, useContext } from "react";
+import ForceGraph3D from "react-force-graph-3d";
+import SpriteText from "three-spritetext";
+import data from "./data.json";
+import * as d3 from "d3";
+import { GeneralContext } from "./context/context";
+import ResponsiveTable from "./utils/ResponsiveTable";
+import TableInfo from "./utils/TableInfo";
 
-function App() {
+export default function App() {
+
+  const { fgRef,
+    flag, setFlag,
+    nodeThreeObject,
+    handleClick,
+    handleBackgroundClick,
+    drawCategoryNode,
+    drawNormalNode
+  } = useContext(GeneralContext)
+
+
+  useEffect(() => {
+    fgRef.current.d3Force("link").distance((link) => {
+      return link.distance ? link.distance * 10 : 60;
+    });
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home" >
+
+      <div className="graph-container" >
+        <h2 className="graph-label">Graph  Visualisation</h2>
+        <ForceGraph3D
+          showNavInfo={false}
+          ref={fgRef}
+          backgroundColor="#fff"
+          graphData={data}
+          nodeAutoColorBy="group"
+          nodeThreeObject={nodeThreeObject}
+          nodeVal={(node) => node.size}
+          linkColor={"color"}
+          linkOpacity={1}
+          linkWidth={"width"}
+          onNodeClick={handleClick}
+          onBackgroundClick={handleBackgroundClick}
+          onNodeDragEnd={(node) => {
+            node.fx = node.x;
+            node.fy = node.y;
+            node.fz = node.z;
+          }}
+          width={750}
+          height={500}
+        />
+      </div>
+
+      <div className="table-container" >
+        <div dir="rtl" className="table-content" >
+          <h2>Nodes </h2>
+          <ResponsiveTable />
+        </div>
+        <div dir="rtl" className="table-content" >
+          <h2>Node Informations</h2>
+          <TableInfo />
+        </div>
+
+
+
+      </div>
     </div>
+
   );
 }
-
-export default App;
